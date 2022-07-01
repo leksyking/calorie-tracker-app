83d533d8e4459435e77857a14578f20587f6e186
Leksyking <gbemilekeogundipe@gmail.com>
2022-07-01 14:30:36 -0700

    add mongodb connection

---
+++ server/routes/connection.go
@@ -0,0 +1,34 @@
+package routes
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBInstance() *mongo.Client {
+	Mongodb := "mongodb://0.0.0.0:27017/caloriesdb"
+	client, err := mongo.NewClient(options.Client().ApplyURI(Mongodb))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB")
+	return client
+}
+
+var Client *mongo.Client = DBInstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("caloriesdb").Collection(collectionName)
+	return collection
+}
