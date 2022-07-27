871901f60bc98a8e1c9cba28e003082aced6f013
Leksyking <gbemilekeogundipe@gmail.com>
2022-07-27 13:48:44 -0700

    add entry functions, buttons and other entries

--- frontend/src/components/single-entry.component.js
+++ frontend/src/components/single-entry.component.js
@@ -9,7 +9,32 @@ const Entry = ({entryData, setChangeIngredient, deleteSingleEntry, setChangeEntr
         <Card>
              <Row>
                 <Col>Dish:{entryData !== undefined && entryData.dish}</Col>
+                <Col>Ingredients:{entryData !== undefined && entryData.ingredients}</Col>
+                <Col>Calories:{entryData !== undefined && entryData.calories}</Col>
+                <Col>Fat:{entryData !== undefined && entryData.fat}</Col>
+                <Col><Button onClick={()=> deleteSingleEntry(entryData._id) }>delete entry</Button></Col>
+                <Col><Button onClick={()=> changeIngredient() }>change ingredients</Button></Col>
+                <Col><Button onClick={()=> changeEntry() }>change entry</Button></Col>
              </Row>
         </Card>
     )
+
+    function changeIngredient(){
+        setChangeIngredient(
+            {
+                "change": true,
+                "id": entryData._id
+            }
+        )
+    }
+
+    function changeEntry(){
+        setChangeEntry(
+            {
+               "change": true,
+               "id": entryData._id
+            }
+        )
+    }
 }
+
