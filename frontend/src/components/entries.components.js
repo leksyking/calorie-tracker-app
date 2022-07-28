861b381783e81070ce8f78974de0355e2cbe765a
Leksyking <gbemilekeogundipe@gmail.com>
2022-07-28 09:38:23 -0700

    add and delete entry functions

--- frontend/src/components/entries.components.js
+++ frontend/src/components/entries.components.js
@@ -5,3 +5,42 @@ import axios from 'axios';
 import {Button, Form, Container, Modal} from 'react-bootstrap';
 
 import Entry from  './single-entry.component';
+
+const Entries = ()=>{
+    return (
+        <div>
+            <Container>
+                <Button onClick = {()=> setAddNewEntry(true)}>Track today's calories</Button>
+            </Container>
+            <Container>
+                {entries != null && entries.map((entry, i) => (
+                    <Entry entryData={entry} deleteSingleEntry={deleteSingleEntry} setChangeIngredient={setChangeIngredient} setChangeEntry={setChangeEntry}/>
+                ))}
+            </Container>
+        </div>
+    );
+}
+
+function addSingleEntry(){
+    setAddNewEntry(false)
+    var url = "http://localhost:3000/entry/create"
+    axios.post(url, {
+        "ingredients": newEntry.ingredients,
+        "dish": newEntry.dish,
+        "calories": newEntry.calories,
+        "fat": newEntry.fat
+    }).then(response =>{
+        if(response.status == 200){
+            setRefreshData(true)
+        }
+    })
+}
+
+function deleteSingleEntry(id){
+    var url = "http://localhost:3000/entry/delete" + id
+    axios.delete(url, ).then(response =>  {
+        if (response.status == 200) {
+
+        }
+    })
+}
