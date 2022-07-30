ac612650e6531229237029d68e24eeaad18ffaed
Leksyking <gbemilekeogundipe@gmail.com>
2022-07-30 03:43:52 -0700

    add get all entries

--- frontend/src/components/entries.components.js
+++ frontend/src/components/entries.components.js
@@ -7,6 +7,24 @@ import {Button, Form, Container, Modal} from 'react-bootstrap';
 import Entry from  './single-entry.component';
 
 const Entries = ()=>{
+
+    const [entries, setEntries] = useState([])
+    const [refreshData, setRefreshData] = useState(false)
+    const [changeEntry, setChangeEntry] = useState({"change": false, "id": 0 })
+    const [changeIngredient, setChangeIngredient] =  useState({"change": false, "id": 0})
+    const [newIngredientName, setNewIngredientName] = useState("")
+    const [addNewEntry, setAddNewEntry] = useState(false)
+    const [newEntry, setNewEntry] = useState({"dish": "", "ingredients":"", "calories":0, "fat":0})
+
+    useEffect(() =>{
+        getAllEntries();
+    }, [])
+
+    if (refreshData){
+        setRefreshData(false);
+        getAllEntries();
+    }
+
     return (
         <div>
             <Container>
@@ -40,7 +58,18 @@ function deleteSingleEntry(id){
     var url = "http://localhost:3000/entry/delete" + id
     axios.delete(url, ).then(response =>  {
         if (response.status == 200) {
+            setRefreshData(true)
+        }
+    })
+}
 
+function getAllEntries(){
+    var url = "http://localhost:3000/entries"
+    axios.get(url, {
+        responseType: "json"
+    }).then(response => {
+        if (response.status == 200){
+            setEntries(response.data)
         }
     })
 }
